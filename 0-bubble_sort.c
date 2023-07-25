012c8382e45347cb15d3f7040498a9d57e6caa39
victormayowa <hardmanvicky01@gmail.com>
2023-07-25 07:05:43 -0700

    correct  bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,30 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
  *
- * Description: Prints the array after each swap.
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t a, ln = size;
-	bool bub = false;
+    size_t i, j;
+    int temp;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	while (bub == false)
-	{
-		bub = true;
-		for (i = 0; i < ln - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bun = false;
-			}
-		}
-		ln--;
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
 }
--- sort.h
+++ sort.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
 /* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
