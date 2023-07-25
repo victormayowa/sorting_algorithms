918a21f7b3d6d604f8756da7d66dbda0d938aff2
victormayowa <hardmanvicky01@gmail.com>
2023-07-25 07:16:43 -0700

    correct insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,53 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in
- * ascending order using the Insertion sort algorithm
+ * swap_nodes - Swaps two nodes in a doubly linked list
  *
- * @list: Double pointer to the head of the list
+ * @a: Pointer to the first node
+ * @b: Pointer to the second node
+ */
+void swap_nodes(listint_t **a, listint_t **b)
+{
+    listint_t *tmp_prev = (*a)->prev;
+    listint_t *tmp_next = (*a)->next;
+
+    if (tmp_prev)
+        tmp_prev->next = *b;
+    (*a)->next = (*b)->next;
+    (*a)->prev = *b;
+    if ((*a)->next)
+        (*a)->next->prev = *a;
+    (*b)->next = *a;
+    (*b)->prev = tmp_prev;
+    if (tmp_next)
+        tmp_next->prev = *a;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion sort algorithm
+ *
+ * @list: The doubly linked list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    listint_t *sorted = NULL;
+    listint_t *current, *tmp;
 
-	listint_t *current = (*list)->next;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	while (current)
-	{
-		listint_t *tmp = current->prev;
-
-		while (tmp && current->n < tmp->n)
-		{
-			tmp->next = current->next;
-			if (current->next)
-				current->next->prev = tmp;
-			current->next = tmp;
-			current->prev = tmp->prev;
-			tmp->prev = current;
-			if (current->prev)
-				current->prev->next = current;
-			else
-				*list = current;
-			tmp = current->prev;
-			print_list(*list);
-		}
-		current = current->next;
-	}
+    current = *list;
+    while (current != NULL)
+    {
+        tmp = current->next;
+        while (current->prev != NULL && current->n < current->prev->n)
+        {
+            swap_nodes(&(current->prev), &current);
+            if (current->prev == NULL)
+                *list = current;
+            print_list(*list);
+        }
+        current = tmp;
+    }
 }
